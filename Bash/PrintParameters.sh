bf5a5a02671ac7e969b6c91af5c46509eb747260
Albin Vázquez <111664806+v4zquez@users.noreply.github.com>
2024-05-13 14:54:00 -0600

    Add files via upload

---
+++ Bash/PrintParameters.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Program to print the value of parameters passed to the bash script 
+
+echo "Number of parameters: $#"
+
+lastId=$#
+parametersNumber=${@:lastId}
+
+if [[ $# -le 4 ]]; then
+    echo "The number of parameters must be greater than 4"
+    exit 1
+fi
+
+if [[ $parametersNumber -gt $# ]]; then
+    echo "The required quantity exceeds the number of parameters"
+    exit 2
+fi
+
+if [[ $# -eq $parametersNumber ]]; then
+    parametersNumber=$parametersNumber-1
+fi
+
+declare -i i=1
+
+while [[ $i -le $parametersNumber ]]; do
+    var=""; var="$"; var+="$i"
+    echo -n "Value of parameter $i is: "
+    outputMessage=$(eval $var 2>&1) # The eval statement tells the shell to take eval's arguments as commands and 
+                                    # run them through the command-line
+    echo $outputMessage | awk '{print $4}' | tr -d ":" 
+    let i=1+$i
+done
